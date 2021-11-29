2f0182c9f73c48a467a53e50d220887d3ca4ac72
山崎剛 <tsuyoshi_yamazaki@respawn.jp>
2021-11-29 23:58:58 +0900

    変更ナシ状態

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    タイトル
+                    Laravel
                 </div>
 
                 <div class="links">
