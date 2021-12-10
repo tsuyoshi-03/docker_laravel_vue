b00d878173d0fa497ef64352692be1af52f49b25
山崎剛 <tsuyoshi_yamazaki@respawn.jp>
2021-12-10 17:49:02 +0900

    auth実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-Route::get('/','PagesController@index')->name('project_Q&A');
+//Route::get('/','PagesController@index')->name('project_Q&A');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
