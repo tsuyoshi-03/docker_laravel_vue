c91326a1b8fd84b7ef942943e8fca448868c4ec4
山崎剛 <tsuyoshi_yamazaki@respawn.jp>
2022-04-08 23:55:12 +0900

    ルーティング修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -26,13 +26,9 @@ Route::middleware(['auth','verified'])->group(function(){
     Route::get('user/mypage', 'UserController@mypage')->name('user.mypage');
     //各ユーザーの投稿ページ
     Route::get('user/{user}', 'UserController@show')->name('user.show');
-
-
+    //ユーザーネーム編集
     Route::get('name/{user}/edit', 'UserController@name_edit')->name('name.edit');
     Route::put('name/{user}', 'UserController@name_update')->name('name.update');
-
-    //Route::get('user/{user}/edit', 'UserController@edit')->name('user.edit');
-    //Route::put('user/{user}', 'UserController@update')->name('user.update');
 
     Route::resource('topics', 'TopicController');
     Route::resource('topics.comments', 'CommentController');
