7f53f669c00a9f34dd07742291e122ccc177cad4
山崎剛 <tsuyoshi_yamazaki@respawn.jp>
2022-04-22 18:21:54 +0900

    admin/registerController ログアウト処理追加

--- src/app/Http/Controllers/Admin/Auth/LoginController.php
+++ src/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -47,5 +48,17 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         return view('admin.auth.login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        return $this->loggedOut($request);
+    }
+
+    public function loggedOut(Request $request)
+    {
+        return redirect(route('admin.login'));
     }
 }
